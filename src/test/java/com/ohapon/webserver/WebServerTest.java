8f49cae731d67121535a549e01d82a1973a014f9
ohapon <ohapon@gmail.com>
2023-03-29 12:27:03 +0300

    Add test

--- src/test/java/com/ohapon/webserver/WebServerTest.java
+++ src/test/java/com/ohapon/webserver/WebServerTest.java
@@ -1,2 +1,51 @@
-package com.ohapon.webserver;public class WebServerTest {
+package com.ohapon.webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+public class WebServerTest {
+
+    @Test
+    public void testSendMessage() throws IOException {
+        //float x = 123456789012345678901234567890.1234567890f;
+        //double y = 123456789012345678901234567890.1234567890d;
+
+        float x = 12345.4567f;
+        double y = 123456789012.456789d;
+
+        DecimalFormat format = new DecimalFormat("#.####");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(format.format(x));
+        System.out.println(format.format(y));
+
+        WebServer server = new WebServer();
+
+        Thread thread = new Thread(() -> {
+            try {
+                server.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        Client client = new Client();
+//        String response = client.sendMessage("Hi!");
+//        server.stop();
+//
+//        assertEquals("Echo: Hi!", response);
+
+    }
+
 }
